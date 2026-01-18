45bf69ee6d0c8dc38db387beabd19752fb134d5b
balaji-borude <balajiborude2503@gmail.com>
2026-01-18 13:21:54 +0530

    Changing setup from next to react and Express.js

---
+++ Backend/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Ts File working runnign");
+//# sourceMappingURL=index.js.map
---
+++ Backend/src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript is alive");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
