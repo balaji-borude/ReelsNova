eded666369abe38357f764e27e5c0000aee331a0
balaji-borude <balajiborude2503@gmail.com>
2026-01-26 10:38:39 +0530

    Implementing the Feed section / Swiper

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 const Feed = () => {
-  return (
-    <div className='text-white'>This is the Feed </div>
-  )
+  return<div className='text-white flex'>
+
+  <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  </div>
 }
 
 export default Feed
